7f8b283c1950d73d99bff66179f7f1cad902e343
JJpink31 <140741161+JJpink31@users.noreply.github.com>
2023-09-29 20:50:14 -0500

    Added moment() to js and omitted date and time from html

--- src/index.js
+++ src/index.js
@@ -1,13 +1,36 @@
-setInterval(() => {
-    d = new Date(); //object of date()
-    hr = d.getHours();
-    min = d.getMinutes();
-    sec = d.getSeconds();
-    hr_rotation = 30 * hr + min / 2; //converting current time
-    min_rotation = 6 * min;
-    sec_rotation = 6 * sec;
- 
-    hour.style.transform = `rotate(${hr_rotation}deg)`;
-    minute.style.transform = `rotate(${min_rotation}deg)`;
-    second.style.transform = `rotate(${sec_rotation}deg)`;
-}, 1000);
+function updateTime(){
+let sanJuanElement=document.querySelector("#sanJuan");
+let sanJuanDateElement=sanJuanElement.querySelector(".date");
+let sanJuanTimeElement=sanJuanElement.querySelector(".time");
+let sanJuanTime=moment().tz("America/Argentina/San_Juan");
+ sanJuanDateElement.innerHTML=moment().format("MMMM Do,  YYYY");
+ sanJuanTimeElement.innerHTML=sanJuanTime.format("h:mm:ss [<small>]A[</small>]");
+
+
+let bangkokElement=document.querySelector("#bangkok");
+let bangokokDateElement=bangkokElement.querySelector(".date");
+let bangkokTimeElement=bangkokElement.querySelector(".time");
+let bangkokTime=moment().tz("Asia/Bangkok");
+ bangokokDateElement.innerHTML=moment().format("MMMM Do,  YYYY");
+ bangkokTimeElement.innerHTML=bangkokTime.format("h:mm:ss [<small>]A[</small>]");
+
+
+let newSalemElement=document.querySelector("#newSalem");
+let newSalemDateElement=newSalemElement.querySelector(".date");
+let newSalemTimeElement=newSalemElement.querySelector(".time");
+let newSalemTime=moment().tz("America/North_Dakota/New_Salem");
+  newSalemDateElement.innerHTML=moment().format("MMMM Do,  YYYY");
+  newSalemTimeElement.innerHTML=newSalemTime.format("h:mm:ss [<small>]A[</small>]");
+
+
+let dublinElement=document.querySelector("#dublin");
+let dublinDateElement=dublinElement.querySelector(".date");
+let dublinTimeElement=dublinElement.querySelector(".time");
+let dublinTime=moment().tz("Europe/Dublin");
+  dublinDateElement.innerHTML=moment().format("MMMM Do,  YYYY");
+  dublinTimeElement.innerHTML=dublinTime.format("h:mm:ss [<small>]A[</small>]");
+}
+
+
+updateTime();
+setInterval(updateTime,1000);
